42e26bad16bf4155dbf13bc44d3d8143848964f1
Brayam1993 <argofrog@live.com.mx>
2022-02-23 21:44:53 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# w3_JS_Homework_01
+ Homework_22022302
---
+++ server.js
@@ -0,0 +1,13 @@
+var carName = "Volvo";
+console.log(carName)
+
+var x = 50;
+console.log(x)
+
+var y = 10;
+//En este ejercicio, escribi la linea tal cual para que me mostrara el resultado pero no me lo mostro.
+//document.getElementById("demo").innerHTML=x+y;
+console.log(x+y)
+
+var z = x+y;
+//alert(z);
